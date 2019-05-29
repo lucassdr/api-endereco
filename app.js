ad1a2e553f5caa7b77200139e989fb52f6cd0fb1
Lucas Sodré <lucas.sodre.teixeira@outlook.com>
2019-05-29 16:11:24 -0300

    Retornando JSON após a promise

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const axios = require('axios')
-const googleMapsClient = require('@google/maps')
 const app = express();
 const port = 3000;
 
@@ -11,28 +10,27 @@ app.post('/distancia', (req, res) => {
 
     let addressList = [];
 
-    req.body.map((addressStr) => {
+    let promisesList = req.body.map((addressStr) => {
 
-        
-
-        axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${addressStr}&key=AIzaSyC2mW5fz2VBY9VSztro48UiA1_k1JKdQ9E`)
+        return  axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${addressStr}&key=AIzaSyC2mW5fz2VBY9VSztro48UiA1_k1JKdQ9E`)
             .then(res => {
                 addressList.push({
                     "address": res.data.results[0].formatted_address
                     , "lat": res.data.results[0].geometry.location.lat
                     , "long": res.data.results[0].geometry.location.lng
                 })
-                console.log("========================");
-                console.log("addressList", addressList);
             }).catch(err => {
-                console.log('Error: ' + err)
+                console.log('Error:', err)
             });
-        return true;
 
-    });
+    })
+    
+    Promise.all(promisesList)
+        .then(function (results) {
+        console.log("finalizado", addressList);
+        res.json({addressList});
+    })
 
-    console.log("finalizado", addressList);
-    res.status(200).json(addressList);
 })
 
 app.listen(port, (err) => {
