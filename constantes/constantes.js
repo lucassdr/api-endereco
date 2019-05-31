2fc7f4b79eb4f052fecb6859a8c8d9020507011b
Lucas Sodré <lucas.sodre.teixeira@outlook.com>
2019-05-31 16:10:44 -0300

    Alteradas as constantes

--- constantes/constantes.js
+++ constantes/constantes.js
@@ -1,4 +1,4 @@
-const googleMapsApiKey = 'AIzaSyC2mW5fz2VBY9VSztro48UiA1_k1JKdQ9E';
+const googleMapsApiKey = require('../config/configuracao')
 const urlGoogleMaps = `https://maps.googleapis.com/maps/api/geocode/json?key=${googleMapsApiKey}`;
 
 module.exports = urlGoogleMaps;
